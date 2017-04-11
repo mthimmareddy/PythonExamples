842e8fbcca72190d3ff0994edca0db0620d9f402
Ravi <ravch46@gmail.com>
2017-04-11 07:13:28 -0700

    secondcommit

--- Datatypes/test.py
+++
@@ -1,3 +0,0 @@
-
-for i in range(5,8):
-  print (i)
--- test.py
+++
@@ -1,9 +0,0 @@
-
-program_1='''Write a Program to validate the ip address'''
-test='hello'
-test2="hi der"
-print (test)
-print(test2)
-print (program_1)
-#ans1=input(program_1)
-
