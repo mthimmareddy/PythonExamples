dfc2cafbfbeed7758a67fc26ab160aa034289494
mthimmareddy <manjujnv87@gmail.com>
2017-05-01 13:24:40 -0400

    First commit

---
+++ NumpyExamples.py
@@ -0,0 +1,203 @@
+import numpy as np
+
+
+Programs = '''
+ndarray.ndim
+the number of axes (dimensions) of the array. In the Python world, the number of dimensions is referred to as rank.
+ndarray.shape
+the dimensions of the array. This is a tuple of integers indicating the size of the array in each dimension. For a matrix with 
+n rows and m columns, shape will be (n,m). The length of the shape tuple is therefore the rank, or number of dimensions, ndim.
+ndarray.size
+the total number of elements of the array. This is equal to the product of the elements of shape.
+ndarray.dtype
+an object describing the type of the elements in the array. One can create or specify dtype’s using standard Python types. 
+Additionally NumPy provides types of its own. numpy.int32, numpy.int16, and numpy.float64 are some examples.
+ndarray.itemsize
+the size in bytes of each element of the array. For example, an array of elements of type float64 has itemsize 8 (=64/8),
+ while one of type complex32 has itemsize 4 (=32/8). It is equivalent to ndarray.dtype.itemsize.
+ndarray.data
+the buffer containing the actual elements of the array. Normally, we won’t need to use this attribute because we will access 
+the elements in an array using indexing facilities.
+
+
+1.Numpy Declarations
+2.Basic Operations
+3.Builtin functions in numpy
+4.Accessing the numpy array
+
+'''
+
+print('Programs')
+
+#############################################################################################
+#############################################################################################
+employment = np.array([
+    55.70000076, 51.40000153, 50.5, 75.69999695,
+    58.40000153, 40.09999847, 61.5, 57.09999847,
+    60.90000153, 66.59999847, 60.40000153, 68.09999847,
+    66.90000153, 53.40000153, 48.59999847, 56.79999924,
+    71.59999847, 58.40000153, 70.40000153, 41.20000076
+])
+
+ridership = np.array([
+    [0, 0, 2, 5, 0],
+    [1478, 3877, 3674, 2328, 2539],
+    [1613, 4088, 3991, 6461, 2691],
+    [1560, 3392, 3826, 4787, 2613],
+    [1608, 4802, 3932, 4477, 2705],
+    [1576, 3933, 3909, 4979, 2685],
+    [95, 229, 255, 496, 201],
+    [2, 0, 1, 27, 0],
+    [1438, 3785, 3589, 4174, 2215],
+    [1342, 4043, 4009, 4665, 3033]
+])
+###################################################################################################
+
+def numpy_dec():
+# Subway ridership for 5 stations on 10 different days343q
+
+ a=np.arange(15).reshape(3,5)
+ print(a)
+ print("Shape:{0},dimention:{1},datatype:{2},itemsize:{3},size:{4},type:{5}",
+ print(a.shape,a.ndim,a.dtype.name,a.itemsize,a.size,type(a)))
+
+ b=np.random.rand(3,2) #Create an array of the given shape and populate it with random samples from a uniform distribution over [0, 1).
+ print(b)
+
+a1 = np.arange(6)
+a2=np.arange(15).reshape(5,3)
+a3=np.arange(24).reshape(2,3,4)
+
+print(a1,a2,a3)
+
+################################################################################################################33
+def basic_operations():
+#Arithmetic operators on arrays apply elementwise. A new array is created and filled with the result.
+ a=np.array([4,5,6,7])
+ b=np.arange(4)
+ c=a-b
+ print(c)
+ print(b**2) #square of each elements
+ print(10*np.sin(a))
+ print(a<3) #return the boolen np array
+
+#Matrix multiplication by dot opearator
+ A=np.array([[1,1],[2,3]])
+ B=np.array([[1,0],[5,6]])
+ C1=A.add(B) #adding the eleement
+ C2=np.add(A,B)
+ C=A*B   #Elementwise product
+ D=A.dot(B) #Matrix product
+ E=np.dot(A,B)  #other way of matrix product
+ print(np.sqrt(A))
+ print(np.exp(A))
+
+#Other basic operations as sum,min,max,mean
+
+ a=np.array([1,2,3,4,5,6,7])
+ print(a.sum())#sum of all elements
+ print(a.min()) #minimum element
+ print(a.max())
+ print(a.mean())
+ print(a.argmax()) #returns the index of maximum element
+ print(a.argmin())
+
+#Other useful builtin fuctions
+#all, any, apply_along_axis, argmax, argmin, argsort, average, bincount, ceil, clip, conj, corrcoef, cov, cross, cumprod, cumsum,
+# diff, dot, floor, inner, inv, lexsort, max, maximum, mean, median, min, minimum,
+#  nonzero, outer, prod, re, round, sort, std, sum, trace, transpose, var, vdot, vectorize, where
+
+
+##########################################################################################################
+
+
+
+
+#i Dimenstional numpy array
+ countries = np.array([
+    'Afghanistan', 'Albania', 'Algeria', 'Angola', 'Argentina',
+    'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas',
+    'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium',
+    'Belize', 'Benin', 'Bhutan', 'Bolivia',
+    'Bosnia and Herzegovina'
+ ])
+
+
+# Employment data in 2007 for those 20 countries
+ employment = np.array([
+    55.70000076,  51.40000153,  50.5       ,  75.69999695,
+    58.40000153,  40.09999847,  61.5       ,  57.09999847,
+    60.90000153,  66.59999847,  60.40000153,  68.09999847,
+    66.90000153,  53.40000153,  48.59999847,  56.79999924,
+    71.59999847,  58.40000153,  70.40000153,  41.20000076
+ ])
+#2Dimentional numpy array
+ ridership = np.array([
+    [0, 0, 2, 5, 0],
+    [1478, 3877, 3674, 2328, 2539],
+    [1613, 4088, 3991, 6461, 2691],
+    [1560, 3392, 3826, 4787, 2613],
+    [1608, 4802, 3932, 4477, 2705],
+    [1576, 3933, 3909, 4979, 2685],
+    [95, 229, 255, 496, 201],
+    [2, 0, 1, 27, 0],
+    [1438, 3785, 3589, 4174, 2215],
+    [1342, 4043, 4009, 4665, 3033]
+ ])
+
+ print(countries)
+
+ print(employment[2])
+#####################################################################################################################
+def access_numpy():
+
+    #1 Diementional arrays accessed asa lists
+    print(employment)
+    print(employment[0])
+
+    #Accessing the 2-D numpy array
+    print(ridership[:,:])#all rows,all columns
+    print(ridership[:,2])#all rows 2 column
+    print(ridership[:,1:4])#all rows 1 to 3 columns
+    print(ridership[1:3,:])#1 to 2 rows, all columns
+
+
+#####################################################################################################################3
+def numpy_builtin():
+    print("{0} station has maximum riders on day1 with {1}".format(ridership[0, :].argmax(), ridership[0, :].max()))
+    # print (ridership[0,:].max())
+
+    print(ridership[:, 3].mean())
+
+    # Mean ridership per day for all stations
+    # return maximum and minimum ridership per day among all staions
+    print(ridership.mean(axis=0))  # to get the mean along columns
+    print(ridership.mean(axis=1))  # to get mean along rows
+    print(ridership.argmax(axis=1))
+    print(ridership.argmax(axis=0))
+
+    print(ridership.mean(axis=0).max())
+    print(ridership.mean(axis=0).min())
+
+
+###################################################################################################3
+
+options = {1:numpy_dec,2:basic_operations,3:numpy_builtin,4:access_numpy}
+
+print(Programs)
+
+
+
+key = 'Y'
+
+# ch=int(input('Enter the program Number :'))
+while (key == 'Y') or (key == 'y') or (key == 'yes') or key == 'Yes' or key == 'YES':
+    print("#####################################################\n")
+    ch = int(input('Enter the program Number :'))
+    print("\n")
+    print("#####################################################\n")
+    options[ch]()
+    key = input('Do you want to continue : Press Y/Yes/y/yes or N/No/no/n : ')
+
+if key == 'N' or key == 'n' or key == 'no' or key == 'NO' or key == 'No':
+    os.system('exit')
---
+++ PandasExamples.py
@@ -0,0 +1,259 @@
+import pandas as pd
+import numpy as np
+
+
+Programs = '''
+1.To Declare the pandas series and To basic operations with pandas,checking size and datatypes and to access the elements 
+in series.
+2.Series addtion operation demo
+3.Creating a DataFrame by passing a numpy array, with a datetime index and labeled columns
+and Viewing the data of dataframe
+4.Selecting the dataframe by loc,iloc
+5.Boolean indexing with dataframes
+6.Handling the missing data in dataframe
+7.apply and applymap on Series and df
+
+8.Groupby in dataframes
+'''
+dates = pd.date_range('20130101', periods=6)
+df = pd.DataFrame(np.random.rand(6, 4), index=dates, columns=list('ABCD'))
+
+#########################################################################################################3
+##########################################################################################################33
+def view_df():
+    df = pd.DataFrame(np.random.rand(6, 4), index=[1, 2, 3, 4, 5, 6], columns=list('ABCD'))
+    print(df)
+
+    #head will print first 5 lines of df
+    print("head will print first 5 lines of df",df.head())
+    #tail last 5 rows of df
+    print("tail last 5 rows of df",df.tail())
+    #Describe shows a quick statistic summary of your data
+    print("Describe shows a quick statistic summary of your data",df.describe())
+    #Display the index, columns, and the underlying numpy data
+    print("Display the index, columns, and the underlying numpy data",df.index)
+    print("Display the index, columns, and the underlying numpy data",df.columns)
+    #Display numpy data
+    print("Display numpy data",df.values)
+    #Transposing your data
+    print("Transposing your data",df.T)
+
+    #Sorting by an axis
+    print("Sorting by an axis",df.sort_index(axis=1,ascending=False))
+    #Sorting by values
+    print("Sorting by an values",df.sort_values(by='B'))
+
+##################################################################################################################
+
+def selectdf():
+    #While standard Python / Numpy expressions for selecting and setting are intuitive and come in handy for
+    #interactive work, for production code, we recommend the optimized pandas data access methods, .at, .iat, .loc, .iloc and.ix.
+
+    #Selecting a single column, which yields a Series, equivalent to df.A
+    print("Selecting a single column, which yields a Series, equivalent to df.A",df['A'])
+    #Selecting via [], which slices the rows.
+    print("Selecting via [], which slices the rows.",df[0:3]) #selecting by index
+    print("Selecting via [], which slices the rows.",df['2013-01-01':'2013-01-04'])
+
+    print("############################################\nSHOWING BY LABEL\n#################################################\n")
+
+    #Selection by label
+    print ("Selection by label",df.loc[dates[0]])
+
+
+    #Selecting on a multi-axis by label
+    print("Selecting on a multi-axis by label",df.loc[:,['A','B']])
+    print("Showing label slicing, both endpoints are included",df.loc['2013-01-01':'2013-01-03',['A','B']])
+    print("Reduction in the dimensions of the returned object",df.loc['20130102',['A','B']])
+    print("For getting a scalar value", df.loc[dates[0],'A'])
+    print("For getting fast access to a scalar (equiv to the prior method)",df.at[dates[0],'A'])
+
+    print("############################################\nSHOWING BY POSITION\n#################################################\n")
+
+    print("Select via the position of the passed integers",df.iloc[3])
+    print("By integer slices, acting similar to numpy/python",df.iloc[3:5,0:2])
+    print("By lists of integer position locations, similar to the numpy/python style",df.iloc[[1,2,4],[0,2]])
+    print("For slicing rows explicitly",df.iloc[0:3,:])
+    print("For slicing columns explicitly",df.iloc[:,1:3])
+    print("For getting a value explicitly",df.iloc[0,1])
+    print("For getting fast access to a scalar (equiv to the prior method)",df.iat[0,1])
+
+########################################################################################################################################
+def boolean_index():
+    print("Using a single column’s values to select data.",df[df.A>0])
+    print("A where operation for getting.",df[df>0])
+    print("########################\nUsing the isin() method for filtering\n###########################################")
+    df2=df.copy()
+    df2['E']=['one','two','three','four','five','six']
+    print("isin operation",df2[df2['E'].isin(['two','six'])])
+
+#######################################################################################################################################3
+def missingdata():
+    print("pandas primarily uses the value np.nan to represent missing data")
+
+    print("#################################\nReindexing allows you to change/add/delete the index on a s"
+          "pecified axisThis returns a copy of the data\n########")
+    df1=df.reindex(index=dates[0:4],columns=list(df.columns + ['E']))
+    df1.loc[dates[0]:dates[2],'E']=1
+    print("Dataframe with Nan Values",df1)
+    print("To drop any rows that have missing data.",df1.dropna(how='any'))
+    print("Filling missing data",df1.fillna(value=5))
+    print("To get the boolean mask where values are nan",df1.isnull())
+
+
+#########################################################################################################################################3
+
+#####################################################################################################################
+def series_addtion():
+    s1 = pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
+    s2 = pd.Series([20, 30, 40, 50], index=['a', 'b', 'c', 'd'])
+
+    print(s1 + s2)
+
+    s1 = pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
+    s2 = pd.Series([20, 30, 40, 50], index=['b', 'd', 'a', 'c'])
+
+    print(s1 + s2)
+
+    s1 = pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
+    s2 = pd.Series([20, 30, 40, 50], index=['c', 'd', 'e', 'f'])
+
+    print(s1 + s2)
+
+    s1 = pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
+    s2 = pd.Series([20, 30, 40, 50], index=['e', 'f', 'g', 'h'])
+
+    print(s1 + s2)
+
+#################################################################################################################
+
+def dec_Series():
+
+    s=pd.Series([7,2,3,1,5,9],index=list('ABCDEF'))
+
+    print(s)
+
+    s1 = pd.Series([7, 2, 3, 1, 5, 9], index=list('ABCDEF'))
+
+    countries = pd.Series(['Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda',
+                           'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan',
+                           'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus',
+                           'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia'])
+
+    life_expectancy_values = pd.Series([74.7, 75., 83.4, 57.6, 74.6, 75.4, 72.3, 81.5, 80.2,
+                                        70.3, 72.1, 76.4, 68.1, 75.2, 69.8, 79.4, 70.8, 62.7,
+                                        67.3, 70.6])
+    #0,1,2,3 represents the column names
+
+    df = pd.DataFrame({
+        0: [11, 2, 30, 40],
+        1: [50, 40, 90, 80],
+        2: [90, 110, 100, 120],
+        3: [130, 140, 150, 160]
+    })
+#other way od Dictinary
+
+    df1=pd.DataFrame([[1,2,3,4],[3,6,9,12],[4,8,12,16]],index=['row1','row2','row3'],columns=['A','B','C','D'])
+
+    df1.apply(np.sum,axis=0)
+    df1.apply(np.sum,axis=1)
+    print(df1)
+
+    print(countries)
+    print(life_expectancy_values)
+
+   #print shape of series
+    print(s.shape)
+    print(s[s>3]) #prints the elements greater than 3
+    print(s>3) #Generates the boolen series
+
+    #Vectorized operations on series
+    print(s.add(2))
+    print(s.sub(2))
+    print(s.mul(2))
+    print(s.div(2))
+   #other way of oerations
+
+    print(s+5)
+    print(s-5)
+    print(s*5)
+    print(s/5)
+
+    #Accessing the elements of series
+
+    print(s[0])
+    print(s.loc['C'])
+    print(s.iloc[0:3]) #print range of values
+    print(s[0:4])
+
+##########################################################################################################3
+
+    #apply  method on Series on series
+def squares(x):
+    return(x*x)
+
+    #print("Squares of Series are {0}".format(s.apply(squares)))
+    '''
+    df = pd.DataFrame({
+        0: [11, 2, 30, 40],
+        1: [50, 40, 90, 80],
+        2: [90, 110, 100, 120],
+        3: [130, 140, 150, 160]
+    })
+
+    df1 = pd.DataFrame([[1, 2, 3, 4], [3, 6, 9, 12], [4, 8, 12, 16]], index=['row1', 'row2', 'row3'],
+                       columns=['A', 'B', 'C', 'D'])'''
+################################################################################################################33
+#Apply function on DataFrame acts on every column
+def sec_large(s):
+    s.sort_values(axis=0,ascending=False,inplace='True')
+    return s.iloc[1]
+############################################################################################################33
+    #sec_large(s1)
+def apply_applymap():
+    s = pd.Series([7, 2, 3, 1, 5, 9], index=list('ABCDEF'))
+
+    df = pd.DataFrame({
+        0: [11, 2, 30, 40],
+        1: [50, 40, 90, 80],
+        2: [90, 110, 100, 120],
+        3: [130, 140, 150, 160]
+    })
+
+    print("Squares of Series are {0}".format(s.apply(squares)))
+
+    df1 = pd.DataFrame([[1, 2, 3, 4], [3, 6, 9, 12], [4, 8, 12, 16]], index=['row1', 'row2', 'row3'],
+                       columns=['A', 'B', 'C', 'D'])
+
+    df1.apply(np.sum, axis=0)
+    df1.apply(np.sum, axis=1)
+    print(df1)
+
+    print("Second largest element is {0}".format(df.apply(sec_large,axis=0)))
+
+    #applymap on the dataframe acts on all elements of dataframe
+
+    print("Squares of the dataframe",df.applymap(squares))
+
+##############################################################################################3
+
+
+options = {1:dec_Series,2:series_addtion,3:view_df,4:selectdf,5:boolean_index,6:missingdata,7:apply_applymap}
+
+print(Programs)
+
+
+
+key = 'Y'
+
+# ch=int(input('Enter the program Number :'))
+while (key == 'Y') or (key == 'y') or (key == 'yes') or key == 'Yes' or key == 'YES':
+    print("#####################################################\n")
+    ch = int(input('Enter the program Number :'))
+    print("\n")
+    print("#####################################################\n")
+    options[ch]()
+    key = input('Do you want to continue : Press Y/Yes/y/yes or N/No/no/n : ')
+
+if key == 'N' or key == 'n' or key == 'no' or key == 'NO' or key == 'No':
+    os.system('exit')
